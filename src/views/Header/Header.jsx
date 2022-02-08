06586b420636730b7e36b573721efc516ac7ba51
Ian Christiansen <ianc.songdog@gmail.com>
2022-02-07 16:10:01 -0800

    added message to header

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -3,9 +3,11 @@ import { useUser } from '../../context/UserContext';
 
 export default function Header() {
   const { user } = useUser();
+
+  const welcome = user ? `hi, ${user}!` : 'welcome!';
   return (
     <div>
-      <h1>hi, {user}!</h1>
+      <h1>{welcome}</h1>
     </div>
   );
 }
