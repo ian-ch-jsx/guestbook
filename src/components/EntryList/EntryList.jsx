444d13611af4747a0f8ddd3e5f590ac02b6a5445
Ian Christiansen <ianc.songdog@gmail.com>
2022-02-09 15:38:21 -0800

    adding css for color modes

--- src/components/EntryList/EntryList.jsx
+++ src/components/EntryList/EntryList.jsx
@@ -1,11 +1,13 @@
 import { useEntries } from '../../context/EntryContext';
 import Entry from '../Entry/Entry';
+import { useTheme } from '../../context/ThemeContext';
 import './EntryList.css';
 
 export default function EntryList() {
+  const { theme } = useTheme();
   const { entries } = useEntries();
   return (
-    <div className="entry-list">
+    <div className="entry-list" data-theme={theme}>
       {entries.map((entry) => (
         <span key={entry.id}>
           <Entry entry={entry} />
