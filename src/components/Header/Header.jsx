0881c2e7b08e4934ca01846178708a1c5d936508
Ian Christiansen <ianc.songdog@gmail.com>
2022-02-09 16:22:51 -0800

    small change for netlify

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   const { user } = useUser();
   const { theme } = useTheme('');
 
-  const welcome = user ? `hi, ${user}!` : 'sign the guestbook';
+  const welcome = user ? `Hi, ${user}!` : 'sign the guestbook';
   return (
     <div className="header" data-theme={theme}>
       <span className="spaceholder"></span>
